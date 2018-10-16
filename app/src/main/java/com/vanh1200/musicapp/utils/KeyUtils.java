752aa16ff98136317152864a4dad2644a00fd102
Kiều Việt Anh <35452119+Vanh1200@users.noreply.github.com>
2018-10-16 15:02:31 +0700

    skip staged area

--- app/src/main/java/com/vanh1200/musicapp/utils/KeyUtils.java
+++ app/src/main/java/com/vanh1200/musicapp/utils/KeyUtils.java
@@ -3,5 +3,5 @@ package com.vanh1200.musicapp.utils;
 public interface KeyUtils {
     String KEY_ALBUM_ID = "key_album_id";
     String KEY_SONG_ARR = "key_album_arr";
-    String KEY_SONG_POSITION = "key_album_position";
+    String KEY_SONG_POSITION = "key_album_position"; 
 }
